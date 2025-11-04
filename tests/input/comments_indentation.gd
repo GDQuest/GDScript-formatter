4fea64a8b31eb13bad7f3dcc19ecef0ddbf8b5ac
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2025-11-04 11:14:20 +0100

    Add test case for indented comment at the end of a function body
    Close #156

--- tests/input/comments_indentation.gd
+++ tests/input/comments_indentation.gd
@@ -7,3 +7,11 @@ func test() -> void:
 
 func test2():
 	pass # This comment should stay here too
+
+func test3() -> void:
+	print("Test")
+	# This comment should stay at the end of the function
+
+# This comment should stay above the function
+func test4() -> void:
+	print("Test")
