2ea3d8d6ce4c272939ca16b76c8fb7139f8a7f48
David M. Lary <dmlary@gmail.com>
2026-04-05 23:51:46 -0500

    Indent backslash continuation lines according to style guide
    From gdscript style guide[^1]:
    > Use 2 indent levels to distinguish continuation lines from regular code blocks.
    
    Currently the formatter indents continuation lines to the same level as
    the first line:
    
    ```gdscript
    func test(thing):
        thing.calling(1) \
        .a(12314) \
        .long() \
        .chain("a string breaks things") \
        .of() \
        .functions()
    ```
    
    With this change, continuation lines (joined with a backslash) are
    formatted with double indention:
    
    ```gdscript
    func test(thing):
        thing.calling(1) \
                .a(12314) \
                .long() \
                .chain("a string breaks things") \
                .of() \
                .functions()
    ```
    
    fixes #180
    
    [^1]: https://docs.godotengine.org/en/stable/tutorials/scripting/gdscript/gdscript_styleguide.html#indentation

---
+++ tests/input/line_continuation_indent.gd
@@ -0,0 +1,15 @@
+var thing = \
+my_long_function()
+
+thing \
+.calling(1) \
+.a(12314) \
+.long() \
+.chain("a string breaks things") \
+.of() \
+.functions()
+
+var string = "\
+	this is a really long \
+	string, but we don't want \
+	to change the indentation."
