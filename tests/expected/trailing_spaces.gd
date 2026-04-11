4606423a5889d6c9805f3a0ca81284aff65dfdc2
Nathan Lovato <12694995+NathanLovato@users.noreply.github.com>
2026-04-11 12:49:56 +0200

    fix tests for trailing spaces

--- tests/expected/trailing_spaces.gd
+++ tests/expected/trailing_spaces.gd
@@ -1,11 +1,9 @@
-## This function is the best in the world.
-##
-## There are no mistakes in it.
-func best_function() -> String:
-	var new_string: String = """
+# This test ensures that trailing white space is preserved in a multi-line
+# string after the opening mark and content inside the string, but gets removed
+# after the closing multiline string mark and other statements.
+func test_trailing_spaces() -> String:
+	var new_string: String = """  
 	This is a multi-line string with trailing spaces.  
 	"""
 
 	return new_string
-
-# EOF
