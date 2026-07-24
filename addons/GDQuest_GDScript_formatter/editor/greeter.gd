1a198b928ed8e2bbd1b42256b281cedfe7d8780c
Nathaniel Blackburn <support@nblackburn.uk>
2026-07-24 23:03:44 +0100

    add basic greeter ui
    first version is limited to just showing addon and formatter versions

---
+++ addons/GDQuest_GDScript_formatter/editor/greeter.gd
@@ -0,0 +1,21 @@
+@tool
+class_name Greeter
+extends Node
+
+@onready var _addon_version_label_value: Label = $Padding/Layout/Versions/Addon/Layout/Meta/Value
+@onready var _formatter_version_value: Label = $Padding/Layout/Versions/Formatter/Layout/Meta/Value
+
+var _addon_version: String = ""
+var _formatter_version: String = ""
+
+func _ready() -> void:
+	_addon_version_label_value.text = _addon_version
+	_formatter_version_value.text = _formatter_version
+
+func set_addon_version(version: String) -> void:
+	_addon_version = version
+
+
+func set_formatter_version(version: String) -> void:
+	_formatter_version = version
+	
