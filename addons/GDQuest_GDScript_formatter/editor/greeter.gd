fdecf14e201463777376adf67d951234b87c94d3
Nathaniel Blackburn <support@nblackburn.uk>
2026-07-26 23:14:28 +0100

    clean up label names

--- addons/GDQuest_GDScript_formatter/editor/greeter.gd
+++ addons/GDQuest_GDScript_formatter/editor/greeter.gd
@@ -4,8 +4,8 @@ extends Window
 
 signal action_pressed(action: String)
 
-@onready var _addon_version_label_value: Label = $Panel/Padding/Layout/Versions/Addon/Layout/Meta/Value
-@onready var _formatter_version_value: Label = $Panel/Padding/Layout/Versions/Formatter/Layout/Meta/Value
+@onready var _addon_version_label: Label = $Panel/Padding/Layout/Versions/Addon/Layout/Meta/Value
+@onready var _formatter_version_label: Label = $Panel/Padding/Layout/Versions/Formatter/Layout/Meta/Value
 @onready var _heading_label: Label = $Panel/Padding/Layout/Heading/Description
 
 @onready var _docs_button: Button = $Panel/Padding/Layout/Links/Docs/HBoxContainer/DocsButton
@@ -17,8 +17,8 @@ var _formatter_version: String = "0.0.0"
 
 
 func _ready() -> void:
-	_addon_version_label_value.text = _addon_version
-	_formatter_version_value.text = _formatter_version
+	_addon_version_label.text = _addon_version
+	_formatter_version_label.text = _formatter_version
 	
 	close_requested.connect(_handle_close)
 	
@@ -33,14 +33,14 @@ func set_addon_version(version: String) -> void:
 	_addon_version = version
 	
 	if is_node_ready():
-		_addon_version_label_value.text = _addon_version
+		_addon_version_label.text = _addon_version
 
 
 func set_formatter_version(version: String) -> void:
 	_formatter_version = version
 	
 	if is_node_ready():
-		_formatter_version_value.text = _formatter_version
+		_formatter_version_label.text = _formatter_version
 
 
 func _update_tagline() -> void:
