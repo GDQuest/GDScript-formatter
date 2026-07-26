b7dbe9e6547db144efa952377c192865dc36bb24
Nathaniel Blackburn <support@nblackburn.uk>
2026-07-26 20:09:19 +0100

    rework greeter to use window instead of popup panel

--- addons/GDQuest_GDScript_formatter/editor/greeter.gd
+++ addons/GDQuest_GDScript_formatter/editor/greeter.gd
@@ -1,16 +1,21 @@
 @tool
 class_name Greeter
-extends Node
+extends Window
 
-@onready var _addon_version_label_value: Label = $Padding/Layout/Versions/Addon/Layout/Meta/Value
-@onready var _formatter_version_value: Label = $Padding/Layout/Versions/Formatter/Layout/Meta/Value
+@onready var _addon_version_label_value: Label = $Panel/Padding/Layout/Versions/Addon/Layout/Meta/Value
+@onready var _formatter_version_value: Label = $Panel/Padding/Layout/Versions/Formatter/Layout/Meta/Value
 
 var _addon_version: String = "0.0.0"
 var _formatter_version: String = "0.0.0"
 
+func _init() -> void:
+	close_requested.connect(_handle_close)
+
+
 func _ready() -> void:
 	_addon_version_label_value.text = _addon_version
 	_formatter_version_value.text = _formatter_version
+
 
 func set_addon_version(version: String) -> void:
 	_addon_version = version
@@ -18,4 +23,7 @@ func set_addon_version(version: String) -> void:
 
 func set_formatter_version(version: String) -> void:
 	_formatter_version = version
-	
+
+
+func _handle_close() -> void:
+	hide()
