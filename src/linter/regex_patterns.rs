88df3100e9a402184c900bf6958a68cfc546b738
Artemy Fedotov <catlovescpp@gmail.com>
2026-09-14 21:08:04 +0400

    lint: Allow signal names that start with `_` to be considered as snake_case

--- src/linter/regex_patterns.rs
+++ src/linter/regex_patterns.rs
@@ -11,7 +11,7 @@ macro_rules! static_regex {
 }
 
 static_regex!(SNAKE_CASE, r"^[a-z][a-z0-9_]*$");
-static_regex!(PRIVATE_SNAKE_CASE, r"^_[a-z][a-z0-9_]*$");
+static_regex!(PRIVATE_SNAKE_CASE, r"^_+[a-z][a-z0-9_]*$");
 static_regex!(PASCAL_CASE, r"^[A-Z][a-zA-Z0-9]*$");
 static_regex!(CONSTANT_CASE, r"^[A-Z][A-Z0-9_]*$");
-static_regex!(PRIVATE_CONSTANT_CASE, r"^_[A-Z][A-Z0-9_]*$");
+static_regex!(PRIVATE_CONSTANT_CASE, r"^_+[A-Z][A-Z0-9_]*$");
